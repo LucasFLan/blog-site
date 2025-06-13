e7c57325c13853aa68d91e7613dcf11af32ed172
Lucas Ferreira Lan <lucaslan10@hotmail.com>
2025-06-13 12:17:01 -0300

    Melhorias gerais
    Criação do componente de avatar com Compound pattern, continuação do desenvolvimento do post individual

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,3 +1,6 @@
+import { Avatar } from "@/components/avatar";
+import { AvatarContent } from "@/components/avatar/avatar-content";
+import { Markdown } from "@/components/markdown";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -15,40 +18,69 @@ export default function PostPage() {
   const router = useRouter();
 
   const slug = router.query.slug as string;
-  const post = allPosts.find((post) =>
-    post.slug.toLowerCase().includes(slug.toLowerCase())
-  );
+
+  if (!slug) return <p>Carregando...</p>;
+
+  const post = allPosts.find(
+    (post) => post.slug.toLowerCase() === slug.toLowerCase()
+  )!;
+
+  const publishedDate = new Date(post?.date).toLocaleDateString("pt-BR");
 
   return (
     <main className="mt-32 text-gray-100">
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink href="/blog" asChild>
-              <Link href="/" className="text-action-sm">
-                Blog
-              </Link>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <span className="text-blue-200 text-action-sm">{post?.title}</span>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
+      <div className="container space-y-12 px-4 md:px-8">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild className="text-action-sm">
+                <Link href="/blog">Blog</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <span className="text-blue-200 text-action-sm">
+                {post?.title}
+              </span>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+        <div className="grid grid-cols-1 lg:grid-cols-[1fr_300px] gap-6 lg:gap-12">
+          <article className="bg-gray-600 rounded-lg overflow-hidden border-gray-400 border-[1px]">
+            <figure className="relative aspect-[16/10] w-full overflow-hidden rounded-lg">
+              <Image
+                src={post?.image ?? ""}
+                alt={post?.title ?? ""}
+                fill
+                className="object-cover"
+              />
+            </figure>
+            <header className="p-4 md:p-6 lg:p-12 pb-0 mt-8 md:mt-12">
+              <h1 className="mb-8 text-balance text-heading-lg md:text-heading-xl lg:text-heading-xl">
+                {post?.title}
+              </h1>
+              <Avatar.Container>
+                <Avatar.Image
+                  src={(post?.author.avatar).trim()}
+                  alt={post?.title}
+                  size="sm"
+                />
+                <Avatar.Content>
+                  <Avatar.Title>{post?.author.name}</Avatar.Title>
+                  <Avatar.Description>
+                    Publicado em {""}
+                    <time dateTime={post.date}>{publishedDate}</time>
+                  </Avatar.Description>
+                </Avatar.Content>
+              </Avatar.Container>
+            </header>
 
-      <div className="grid grid-cols-1 lg:grid-[1fr_300px] gap-6 lg:gap-12">
-        <article className="bg-gray-600 rounded-lg overflow-hidden border-gray-400 border-[1px]">
-          <figure className="relative aspect-[16/10] w-full overflow-hidden rounded-lg">
-            <Image
-              src={post?.image ?? ""}
-              alt={post?.title ?? ""}
-              fill
-              className="object-cover"
-            />
-          </figure>
-        </article>
-      </div>
+            <div className="prose prose-invert max-w-none px-4 mt-12 md:px-6 lg:px-12">
+              <Markdown content={post.body.raw} />
+            </div>
+          </article>
+        </div>
+      </div>{" "}
     </main>
   );
 }
