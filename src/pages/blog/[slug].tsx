b674dfdee56ef312d3e0c5c06c1f29d34c5aab48
Lucas Ferreira Lan <lucaslan10@hotmail.com>
2025-06-12 22:06:39 -0300

    Desenvolvimento do post
    Começo do desenvolvimento da página de post individual

---
+++ src/pages/blog/[slug].tsx
@@ -0,0 +1,54 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { allPosts } from "contentlayer/generated";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function PostPage() {
+  const router = useRouter();
+
+  const slug = router.query.slug as string;
+  const post = allPosts.find((post) =>
+    post.slug.toLowerCase().includes(slug.toLowerCase())
+  );
+
+  return (
+    <main className="mt-32 text-gray-100">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/blog" asChild>
+              <Link href="/" className="text-action-sm">
+                Blog
+              </Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <span className="text-blue-200 text-action-sm">{post?.title}</span>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <div className="grid grid-cols-1 lg:grid-[1fr_300px] gap-6 lg:gap-12">
+        <article className="bg-gray-600 rounded-lg overflow-hidden border-gray-400 border-[1px]">
+          <figure className="relative aspect-[16/10] w-full overflow-hidden rounded-lg">
+            <Image
+              src={post?.image ?? ""}
+              alt={post?.title ?? ""}
+              fill
+              className="object-cover"
+            />
+          </figure>
+        </article>
+      </div>
+    </main>
+  );
+}
